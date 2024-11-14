d1cde58dec9f8f345f85dcf4c9c4e4434bf7d4b6
bouc0131 <joshua.bouchat-chapelet@etudiant.univ-reims.fr>
2024-11-14 15:40:31 +0100

    avancement

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,5 @@
 use std::{
+    cmp::Ordering,
     fmt::{Debug, Display},
     ops::{Index, IndexMut},
     usize,
@@ -8,10 +9,11 @@ const INIT: u16 = 0x1FF;
 #[derive(Clone)]
 pub struct Board {
     inner: [u16; 81],
+    to_modifys: Vec<(usize, usize)>,
 }
 fn apply_filter(n: &mut u16, mask: u16) -> bool {
     if n.count_ones() != 1 {
-        let temp = *n ^ mask;
+        let temp = (*n) & mask;
         *n = temp;
         return temp.count_ones() == 1;
     }
@@ -20,82 +22,114 @@ fn apply_filter(n: &mut u16, mask: u16) -> bool {
 
 impl Board {
     fn new() -> Self {
-        Self { inner: [INIT; 81] }
+        Self {
+            inner: [INIT; 81],
+            to_modifys: vec![],
+        }
     }
-    pub fn solve(&mut self) {
-        let mut to_modifys = vec![];
+    pub fn solve(&self) -> Self {
+        let mut init = self.clone();
         for i in 0..9 {
             for j in 0..9 {
-                let temp = self[i][j];
+                let temp = init[i][j];
                 if temp.count_ones() == 1 {
-                    to_modifys.push((i, j));
-                    println!("Pushing: {}:{}", i, j);
+                    init.to_modifys.push((i, j));
                 }
             }
         }
-        println!(
-            "The number of filter to test: {}\n here are the inital grid:\n{:?}",
-            to_modifys.len(),
-            &self
-        );
-        while let Some((i, j)) = to_modifys.pop() {
-            println!("Poping: {}:{}", i, j);
-            self.filter_column(j, i, &mut to_modifys);
-            println!("Column:\n{:?}", &self);
-            self.filter_row(i, j, &mut to_modifys);
-            println!("Rows:\n{:?}", &self);
-            self.filter_block(i, j, &mut to_modifys);
-            println!("Block:\n{:?}", &self);
+        let mut stack = vec![init];
+        let mut res = Board::new();
+        while let Some(mut actual) = stack.pop() {
+            actual.filter_board();
+            res = actual;
+            if res.is_solve() {
+                break;
+            }
+            let (x, y) = self.get_lowest_undefine();
+            let mut val = self[x][y];
+            println!("val[{x}][{y}]: {val}");
+            let mut c = 0;
+            while val > 0 {
+                let temp = val & 1;
+                if temp != 0 {
+                    let mut attempt = self.clone();
+                    attempt[x][y] = 1 << c;
+                    attempt.to_modifys.push((x, y));
+                    println!("{x},{y}: {}", 1 << c);
+                    //println!("{}", &attempt);
+                    //stack.push(attempt);
+                }
+
+                c += 1;
+                val >>= 1;
+            }
+        }
+        return res;
+    }
+    fn get_lowest_undefine(&self) -> (usize, usize) {
+        let mut i = 0;
+        let mut min = 9;
+        let mut min_val = 0x1FF;
+        while min.count_ones() != 2 && i < 81 {
+            let count = min.count_zeros();
+            if count == 1 {
+                i += 1;
+                continue;
+            }
+
+            i += 1;
+        }
+        return min;
+    }
+    fn filter_board(&mut self) {
+        loop {
+            while let Some((i, j)) = self.to_modifys.pop() {
+                self.filter_column(j, i);
+                self.filter_row(i, j);
+                self.filter_block(i, j);
+            }
+            self.filter_block_possibility();
+            self.filter_column_possibility();
+            self.filter_row_possibility();
+            if self.to_modifys.len() == 0 {
+                break;
+            }
         }
     }
-    fn filter_column(
-        &mut self,
-        column_pos: usize,
-        filter_pos: usize,
-        to_modifys: &mut Vec<(usize, usize)>,
-    ) {
-        let mask = self[filter_pos][column_pos];
+
+    fn filter_column(&mut self, column_pos: usize, filter_pos: usize) {
+        let mask = !self[filter_pos][column_pos];
 
         for i in 0..filter_pos {
             if apply_filter(&mut self[i][column_pos], mask) {
-                to_modifys.push((i, column_pos))
+                self.to_modifys.push((i, column_pos))
             }
         }
         for i in (filter_pos + 1)..9 {
             if apply_filter(&mut self[i][column_pos], mask) {
-                to_modifys.push((i, column_pos))
+                self.to_modifys.push((i, column_pos))
             }
         }
     }
-    fn filter_row(
-        &mut self,
-        row_pos: usize,
-        filter_pos: usize,
-        to_modifys: &mut Vec<(usize, usize)>,
-    ) {
-        let mask = self[row_pos][filter_pos];
+    fn filter_row(&mut self, row_pos: usize, filter_pos: usize) {
+        let mask = !self[row_pos][filter_pos];
 
         for i in 0..filter_pos {
             if apply_filter(&mut self[row_pos][i], mask) {
-                to_modifys.push((row_pos, i))
+                self.to_modifys.push((row_pos, i))
             }
         }
         for i in (filter_pos + 1)..9 {
             if apply_filter(&mut self[row_pos][i], mask) {
-                to_modifys.push((row_pos, i))
+                self.to_modifys.push((row_pos, i))
             }
         }
     }
-    fn filter_block(
-        &mut self,
-        filter_posx: usize,
-        filter_posy: usize,
-        to_modifys: &mut Vec<(usize, usize)>,
-    ) {
+    fn filter_block(&mut self, filter_posx: usize, filter_posy: usize) {
         let idx = filter_posx / 3;
         let idy = filter_posy / 3;
 
-        let mask: u16 = self[filter_posx][filter_posy];
+        let mask: u16 = !self[filter_posx][filter_posy];
         for i in 0..3 {
             let row_idx = idx * 3 + i;
             for j in 0..3 {
@@ -104,17 +138,90 @@ impl Board {
                     && column_idy != filter_posy
                     && apply_filter(&mut self[row_idx][column_idy], mask)
                 {
-                    to_modifys.push((row_idx, column_idy))
+                    self.to_modifys.push((row_idx, column_idy))
                 }
             }
         }
     }
+    fn filter_block_possibility(&mut self) {
+        for blk_x in 0..3 {
+            for blk_y in 0..3 {
+                let mut global_mask = 0;
+                let mut global_duplicate = 0;
+                for i in 0..3 {
+                    let row = blk_x * 3 + i;
+                    for j in 0..3 {
+                        let n = self[row][blk_y * 3 + j];
+                        let intersection = global_mask & n;
+                        global_duplicate |= intersection;
+                        global_mask |= n;
+                    }
+                }
+                let unique_mask = global_mask ^ global_duplicate;
+                for i in 0..3 {
+                    let row = blk_x * 3 + i;
+                    for j in 0..3 {
+                        let col = blk_y * 3 + j;
+                        let temp = self[row][col] & unique_mask;
+                        if temp != 0 && self[row][col] != temp {
+                            self[row][col] = temp;
+                            self.to_modifys.push((row, col));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    fn filter_row_possibility(&mut self) {
+        for i in 0..9 {
+            let mut global_mask = 0;
+            let mut global_duplicate = 0;
+            for j in 0..9 {
+                let n = self[i][j];
+                let intersection = global_mask & n;
+                global_duplicate |= intersection;
+                global_mask |= n;
+            }
+            let unique_mask = global_mask ^ global_duplicate;
+            for j in 0..9 {
+                let temp = self[i][j] & unique_mask;
+                if temp != 0 && self[i][j] != temp {
+                    self[i][j] = temp;
+                    self.to_modifys.push((i, j));
+                }
+            }
+        }
+    }
+    fn filter_column_possibility(&mut self) {
+        for j in 0..9 {
+            let mut global_mask = 0;
+            let mut global_duplicate = 0;
+            for i in 0..9 {
+                let n = self[i][j];
+                let intersection = global_mask & n;
+                global_duplicate |= intersection;
+                global_mask |= n;
+            }
+            let unique_mask = global_mask ^ global_duplicate;
+            for i in 0..9 {
+                let temp = self[i][j] & unique_mask;
+                if temp != 0 && self[i][j] != temp {
+                    self[i][j] = temp;
+                    self.to_modifys.push((i, j));
+                }
+            }
+        }
+    }
+
     pub fn transform_back(self, vec: &mut Vec<Vec<char>>) {
         for i in 0..9 {
             for j in 0..9 {
                 vec[i][j] = (((16 - (self[i][j].leading_zeros())) as u8) + '0' as u8) as char
             }
         }
+    }
+    fn is_solve(&self) -> bool {
+        self.inner.iter().all(|i| i.count_ones() == 1)
     }
 }
 impl From<&Vec<Vec<char>>> for Board {
@@ -152,7 +259,7 @@ impl IndexMut<usize> for Board {
 
 fn repr_number_to_char(repr: u16) -> char {
     if repr.count_ones() != 1 {
-        '?'
+        ' '
     } else {
         ((16 - repr.leading_zeros()) as u8 + '0' as u8) as char
     }
@@ -163,16 +270,18 @@ impl Display for Board {
         for i in 0..9 {
             let row = &self[i];
             for j in (0..9).step_by(3) {
-                write!(f, "{} ", repr_number_to_char(row[j]))?;
-                write!(f, "{} ", repr_number_to_char(row[j + 1]))?;
-                write!(f, "{}", repr_number_to_char(row[j + 2]))?;
-                if j != 8 {
+                write!(f, " {} ", repr_number_to_char(row[j]))?;
+                write!(f, " {} ", repr_number_to_char(row[j + 1]))?;
+                write!(f, " {} ", repr_number_to_char(row[j + 2]))?;
+                if j == 0 || j == 3 {
                     write!(f, "|")?;
                 }
             }
-            if i != 8 {
-                write!(f, "\n-----------------\n")?;
+            writeln!(f, "")?;
+            if i == 2 || i == 5 {
+                write!(f, "-----------------------------")?;
             }
+            writeln!(f, "")?;
         }
         writeln!(f)
     }
@@ -182,9 +291,9 @@ impl Debug for Board {
         for i in 0..9 {
             let row = &self[i];
             for j in (0..9).step_by(3) {
-                write!(f, "{:02} ", row[j])?;
-                write!(f, "{:02} ", row[j + 1])?;
-                write!(f, "{:02}", row[j + 2])?;
+                write!(f, "{:03} ", row[j])?;
+                write!(f, "{:03} ", row[j + 1])?;
+                write!(f, "{:03}", row[j + 2])?;
                 if j != 8 {
                     write!(f, "|")?;
                 }
